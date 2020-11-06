2d916a555a2d76e0cd93c36de231be0d69fe5ca2
Joyce Bosco <joycegodinhobosco@gmail.com>
2020-11-05 21:38:40 -0300

    first commit

---
+++ leetcodeRemoveDuplicates.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        i = 0
+        
+        
+        for j in range(1,len(nums)):
+            if nums[j] != nums[i]:
+                i = i + 1
+                nums[i] = nums[j]
+        
+        return i + 1
+
+def main():
+    
+    nums = [1,1,2]
+    print("Teste 1")
+    s = Solution() #chamo a classe
+    print(s.removeDuplicates(nums)) # fl
+    
+    
+    nums2 = [0,0,1,1,1,2,2,3,3,4]
+    print("Teste 2")
+    r = Solution() #chamo a classe
+    print(r.removeDuplicates(nums2)) # ""
+    
+    
+
+    
+main()
