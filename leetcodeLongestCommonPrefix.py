7b7fc8ff2d5867f0e2045bceaf60c8b0173825f5
Joyce Bosco <joycegodinhobosco@gmail.com>
2020-11-05 21:37:36 -0300

    first commit

---
+++ leetcodeLongestCommonPrefix.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        
+        if len(strs)==0:
+            return ""
+        
+        strs.sort() 
+        first = strs[0]
+        last = strs[-1]
+        limite = min(len(first), len(last))
+        resp=""
+        
+        
+        for cont in range(limite):
+            if first[cont] == last[cont]:
+                resp = resp + first[cont]
+                  
+            else:
+                break
+                    
+
+        return resp
+        
+        
+def main():
+    
+    strs = ["flower","flow","flight"] 
+    print("Teste 1")
+    s = Solution() #chamo a classe
+    print(s.longestCommonPrefix(strs)) # fl
+    
+    
+    strs2 = ["dog","racecar","car"]
+    print("Teste 2")
+    r = Solution() #chamo a classe
+    print(r.longestCommonPrefix(strs2)) # ""
+    
+    
+
+    
+#main()
